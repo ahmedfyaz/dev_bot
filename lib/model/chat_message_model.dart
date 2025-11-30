f00120b435060d88e768cb9de4a22b716b3d0297
ahmedfyaz <awanahmed755@gmail.com>
2025-11-30 18:08:15 +0500

    updated chat message model

--- lib/model/chat_message_model.dart
+++ lib/model/chat_message_model.dart
@@ -1,19 +1,43 @@
+class ChatPartModel {
+  final String text;
+
+  ChatPartModel({required this.text});
+
+  factory ChatPartModel.fromJson(Map<String, dynamic> json) {
+    return ChatPartModel(
+      text: json['text'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+    };
+  }
+}
+
 class ChatMessageModel {
   final String role;
   final List<ChatPartModel> parts;
-  ChatMessageModel(
-      {
-        required this.role,
-        required this.parts,
-      }
-      );
-}
-class ChatPartModel {
-  final String text;
-  ChatPartModel(
-      {
-        required this.text,
-      }
-      );
 
+  ChatMessageModel({
+    required this.role,
+    required this.parts,
+  });
+
+  factory ChatMessageModel.fromJson(Map<String, dynamic> json) {
+    return ChatMessageModel(
+      role: json['role'],
+      parts: (json['parts'] as List<dynamic>)
+          .map((e) => ChatPartModel.fromJson(e))
+          .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'role': role,
+      'parts': parts.map((e) => e.toJson()).toList(),
+    };
+  }
 }
